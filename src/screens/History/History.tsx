1adfa169879797a84b20a9cfe25e5e3255816893
Dariosem <dasemenzato@hotmail.com>
2021-09-06 20:57:22 -0300

    created Home and History screens

---
+++ src/screens/History/History.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from './styles';
+
+import { Typography } from '../../components';
+
+const HistoryScreen = () => {
+  return (
+    <View style={styles.mainContainer}>
+      <Typography>History Screen</Typography>
+    </View>
+  );
+};
+
+export default HistoryScreen;
